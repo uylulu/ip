25b7004a8e0df3bcfd44226e1cdf5565c1895175
uylulu <uy.do.trong@gmail.com>
2025-08-27 19:24:23 +0800

    feat: Add, list

--- src/main/java/Uy.java
+++ src/main/java/Uy.java
@@ -1,5 +1,8 @@
 public class Uy {
-    public static String lines = "---------------------------------------";
+    private static String lines = "---------------------------------------";
+    private static String[] messages = new String[100];
+    private static int count = -1;
+
     private static void print(String s) {
         System.out.println(lines);
         System.out.println(s);
@@ -13,11 +16,19 @@ public class Uy {
         print("Hello I'm Uy, what can I do for you?");
         while (true) {
             String message = input();
-            if(message.equals("bye")) {
+            if(message.equals("list")) {
+                for(int i = 0;i <= count;i++) {
+                    System.out.println(i + ". " + messages[i]);
+                }
+                System.out.println(lines);
+            } else if(message.equals("bye")) {
                 print("Bye. Hope to see you again soon!");
                 break;
+            } else {
+                count++;
+                messages[count] = message;
+                print("added: " + message);
             }
-            print(message);
         }
     }
 }
