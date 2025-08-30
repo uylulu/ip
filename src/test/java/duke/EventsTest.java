dc21c99b79b25b020aa1025a15e9cecd305063f1
uylulu <uy.do.trong@gmail.com>
2025-08-30 18:32:19 +0800

    Add JUnit tests

---
+++ src/test/java/duke/EventsTest.java
@@ -0,0 +1,13 @@
+package duke;  //same package as the class being tested
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventsTest {
+    @Test
+    public void testEventCreation() {
+        Events event = new Events("Meeting /from 2025-08-30 /to 2025-08-31");
+        assertEquals(event.toString(), "[E][] Meeting (from: Aug 30 2025 to: Aug 31 2025)");
+    }
+}
